0e87ce63ca8ed6c6c83a084580e185e337255803
Akmal1796 <mhdakmal1796@gmail.com>
2024-09-27 17:47:46 +0530

    Created the basic classes of the project

---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace BaseLibrary.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string? CivilID { get; set; }
+
+        public string? FileNumber { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? JobName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? TelephoneNumber { get; set; }
+
+        public string? Photo { get; set;}
+
+        public string? Other { get; set; }
+
+        //Relationship : Many to One
+        public GeneralDepartment? GeneralDepartment { get; set; }
+        public int GeneralDepartmentID { get; set; }
+
+        public Department? Department { get; set; }
+        public int DepartmentID { get; set; }
+
+        public Branch? Branch { get; set; }
+        public int BranchID { get; set; }
+
+        public Town? Town { get; set; }
+        public int TownID { get; set; }
+    }
+}
---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class Relationship
+    {
+        //Relationship one to many
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}

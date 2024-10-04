02275dcd10dc776fd4ccbc2f3d70f6d79e74aeaf
Akmal1796 <mhdakmal1796@gmail.com>
2024-10-04 20:27:23 +0530

    completed the databae bconnection build

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//starting
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")??
+        throw new InvalidOperationException("Sorry, your connection string is not found!"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
